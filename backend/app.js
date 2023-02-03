98a82d6e0c804e38fd92fd1f332b9a5f9ecd58f4
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2023-02-04 00:31:45 +0700

    set up project

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "*"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+// AUTHENTICATION : SO sánh pass của user so với info trên db, so sánh dữ liệu nhập vs db đã có
+// AUTHORIZATION: bạn là ai và bạn có quyền làm gi (phần quyền)
