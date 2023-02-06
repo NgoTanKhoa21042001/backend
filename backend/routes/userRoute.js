677c39d3e97b7ea01c5871c0fcceb03eb5fcc095
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2023-02-06 08:38:01 +0700

    register with upload avatar

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -7,7 +7,20 @@ const {
 } = require("../controller/userController");
 const router = express.Router();
 
-router.route("/register").post(registerUser);
+const fileUpload = require("express-fileupload");
+const filesPayloadExists = require("../middleware/filePayloadExists");
+const fileExtLimiter = require("../middleware/fileExtLimiter");
+const fileSizeLimiter = require("../middleware/fileSizeLimiter");
+
+router
+  .route("/register")
+  .post(
+    fileUpload({ createParentPath: true }),
+    filesPayloadExists,
+    fileExtLimiter([".png", ".jpg", ".jpeg"]),
+    fileSizeLimiter,
+    registerUser
+  );
 router.route("/login").post(loginUser);
 router.route("/logout").post(logout);
 router.route("/password/update").post(updatePassword);
